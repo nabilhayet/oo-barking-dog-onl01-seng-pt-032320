e79d4f543438b491be76889b7dccf765a133b7ff
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-06 23:59:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Dog 
+def name=dog name 
+@dog_name
+end
+def name 
+  @dog_name
+end 
+
+def bark=dog br 
+end 
+def bark
+
+end 
+
+end 
+
+fido =Dog.new 
+fido.name="Fido"
+puts fido.name 
+fido.bark="woof!"
+puts fido.bark
