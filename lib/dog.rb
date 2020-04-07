ef4e226c3eb05be63df16427a5604d8127f1c1be
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-07 00:10:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog 
+def name=n
+@dog_name=n
+end
+def name 
+  @dog_name
+end 
+
+def bark
+  puts "woof!"
+end 
+end 
+
+fido =Dog.new 
+fido.name="Fido"
+puts fido.name 
+fido.bark
